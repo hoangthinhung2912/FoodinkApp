fd4f5a55200377b0884e16344e6c6024a12995c5
Nhung Hoang <htnhungdhbk@gmail.com>
2018-10-20 22:06:47 +0700

    config

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 //Install express server
 const express = require('express');
 const path = require('path');
@@ -6,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/foodinkApp'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/foodinkApp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
